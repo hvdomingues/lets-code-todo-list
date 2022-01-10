f893b8d5d6c1791ecebe78c7903d0b351ecb95b4
hvdomingues <hvdomingues1@gmail.com>
2022-01-10 11:08:58 -0300

    Alterações das classes Task e Task_List_Class e também da classe Manage_Date. Mudanças nas funções, criado arquivo de testes 'task_list_tests.py' para testes da classe Task_List

--- task_class.py
+++ task_class.py
@@ -1,12 +1,14 @@
+from datetime import datetime
 import pandas as pd
 from manage_csv_class import Manage_Csv
 from category_class import Category
+from task_list_class import Task_List
 
 class Task:
 
     __path = 'task_list.csv'
     
-    def __init__(self, title, date, category, status = 'Pendente'):
+    def __init__(self, title, date=None, category=None, status = 'Pendente'):
         self.title = title
         self.date = date
         self.category = category
@@ -15,37 +17,7 @@ class Task:
     def __repr__(self):
         return f"{self.title} {self.date}"
     
-    @staticmethod
-    def insert_task(*tasks):
-
-        to_insert = pd.DataFrame(columns=Manage_Csv.read(Task.__path).columns)
-
-        for task in tasks:
-            try:
-                Task.__check_task_existence(task)
-                title = task.title
-                date = task.date
-                category_code = Category.find_code(task.category)
-                status = task.status
-                to_insert.loc[-1] = [title,category_code,status,date]
-
-            except Exception as e:
-                print(f"Erro ao inserir a task: {task}\nException: {str(e)}")
-
-            
-
-        Manage_Csv.append_df_to_csv(to_insert, Task.__path)
-        
-    @staticmethod
-    def __check_task_existence(task):
-        existing_tasks = Manage_Csv.read(Task.__path)
-
-        equal_tasks = existing_tasks.loc[(existing_tasks['title'] == task.title) & (existing_tasks['date'] == task.date)].values
-
-        if len(equal_tasks) > 0:
-            print(task)
-            raise Exception('Já existe uma tarefa com o mesmo nome e mesma data.')
             
     @staticmethod
-    def delete_task_by_title():
+    def delete_task_by_title_date():
         pass
---
+++ task_list_tests.py
@@ -0,0 +1,68 @@
+from task_list_class import Task_List
+from task_class import Task
+
+
+# ----------------------------------------------------------------------------------------- #
+# ---------------------------- Testes da classe Task_List --------------------------------- #
+# ----------------------------------------------------------------------------------------- #
+
+print("------------------- Testes de funções para retornar a task_list CSV no formato DataFrame -------------------")
+print("Retorno esperado -> DataFrame de tasks\n")
+print(Task_List.get_task_list())
+
+print("\n\nRetorno esperado -> DataFrame de tasks com nomes de coluna e categoria tratados.\n")
+print(Task_List.get_treated_task_list())
+
+print("\n\n\n------------------- Testes de função para reescrever o DataFrame -------------------")
+print("\n\nRetorno esperado -> ")
+
+print("\n\n\n------------------- Testes de função para procurar objeto Task na Task_List utilizando title ou title e date -------------------")
+print("\n\nTeste 01. Retorno esperado -> Dataframe contendo - Teste,1,Pendente,22-10-1999\n")
+print(Task_List.get_equal_tasks(Task("Teste")))
+
+print("\n\nTeste 02. Retorno esperado -> Dataframe contendo duas tasks com o mesmo nome, datas diferentes.\n")
+print(Task_List.get_equal_tasks(Task("teste de task")))
+
+print("\n\nTeste 03. Retorno esperado -> Lista vazia\n")
+print(Task_List.get_equal_tasks(Task("teste de task de batata assada")))
+
+print("\n\n\n------------------- Testes de função para checar existência da Task na Task_List utilizando title ou title e date -------------------")
+print("\n\nTeste 01. Somente título passado - Retorno esperado -> True\n")
+print(Task_List.check_task_existence(Task("Teste")))
+
+print("\n\nTeste 02. Título e data passado - Retorno esperado -> True\n")
+print(Task_List.check_task_existence(Task("Teste", date="22-10-1999")))
+
+print("\n\nTeste 03. Somente título passado - Retorno esperado -> False\n")
+print(Task_List.check_task_existence(Task("Esse título não existe")))
+
+print("\n\nTeste 04. Título existende e data inexistente passado - Retorno esperado -> False\n")
+print(Task_List.check_task_existence(Task("Teste", date="22-10-1998")))
+
+print("\n\n\n------------------- Testes de função para inserir Task na Task_List ---------------------")
+print("\n ------------------- PRECISA RESETAR O CSV ---------------------")
+print("\n\nTeste 01. Somente uma task passada, não existente - Retorno esperado -> print de sucesso da task adicionada\n")
+task1 = Task("Ler Harry Potter", date="22/10/2022", category="Urgente")
+Task_List.insert_task(task1)
+
+print("\n\nTeste 02. Duas tasks passadas, uma existe e outra não - Retorno esperado -> 1 sucesso e 1 erro\n")
+task1 = Task("Ler Harry Potter", date="22/10/2022", category="Urgente")
+task2 = Task("Aprender Python", date="31/12/1999", category="Urgente")
+Task_List.insert_task(task1, task2)
+
+print("\n\nTeste 03. Duas tasks passadas, uma existente e outra com data incorreta - Retorno esperado -> 2 erros, um devido a data incorreta\n")
+task1 = Task("Ler Harry Potter", date="22/10/2022", category="Urgente")
+task2 = Task("Aprender Python", date="72/12/1959", category="Urgente")
+Task_List.insert_task(task1, task2)
+
+print("\n\nTeste 04. Uma task passada, categoria inexistente - Retorno esperado -> 1 erro devido a categoria inexistente\n")
+task1 = Task("Ler Harry Potter 2", date="22/10/2022", category="Batata")
+Task_List.insert_task(task1)
+
+
+
+
+
+
+
+
