7cfe248028f8d1902ac40ade950531e2e0e6dc8a
hvdomingues <hvdomingues1@gmail.com>
2022-01-10 12:09:07 -0300

    Criada função convert_date para lidar com str ou datetime

--- manage_date_class.py
+++ manage_date_class.py
@@ -1,32 +1,35 @@
 from datetime import date, datetime, timedelta
 import re
+from typing import Type
 from unidecode import unidecode
 
 
 class Manage_Date:
 
     @staticmethod
-    def str_to_date(string_date):
-        if isinstance(string_date, str):
-            if unidecode(string_date.lower()) == "amanha":
-                return Manage_Date.date_to_str(Manage_Date.next_day())
-            elif string_date.lower() == "hoje":
-                return Manage_Date.date_to_str(Manage_Date.today())
+    def convert_date(str_or_date):
+        '''Identify and convert string to datetime or datetime to string'''
+        if isinstance(str_or_date, str):
+            return Manage_Date.str_to_date(str_or_date)
+        elif isinstance(str_or_date, datetime):
+            return Manage_Date.date_to_str(str_or_date)
+        else:
+            raise TypeError('Formato não suportado. Utilizar datetime ou str.')
 
+    @staticmethod
+    def str_to_date(string_date):
+        if unidecode(string_date.lower()) == "amanha":
+            return Manage_Date.next_day()
+        elif string_date.lower() == "hoje":
+            return Manage_Date.today()
+        else:
             list_date = [int(element) for element in re.split('-|/', string_date)]
             list_date.reverse()
             return datetime(*list_date)
-        elif isinstance(string_date, datetime):
-            string_date2 = Manage_Date.date_to_str(string_date)
-            return Manage_Date.str_to_date(string_date2)
 
     @staticmethod
     def date_to_str(date):
-        if isinstance(date, datetime):
-            return format(date.day, '02d')+'/'+format(date.month, '02d')+'/'+format(date.year, '04d')
-        elif isinstance(date, str):
-            date = Manage_Date.str_to_date(date)
-            return format(date.day, '02d')+'/'+format(date.month, '02d')+'/'+format(date.year, '04d')
+        return format(date.day, '02d')+'/'+format(date.month, '02d')+'/'+format(date.year, '04d')
 
     @staticmethod
     def next_day():
